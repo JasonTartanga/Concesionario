99999044415b447382cca65085611a4a50524af0
1dam <1dam@TartangaLH.eus>
2023-03-17 12:38:47 +0100

    Bases subidas

---
+++ recursos/Concesionario.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS CONCESIONARIO;
+CREATE DATABASE CONCESIONARIO;
+USE CONCESIONARIO;
+
+DROP TABLE IF EXISTS USUARIO;
+CREATE TABLE USUARIO(
+	nombre			varchar(25)		not null,
+    contrasenia		varchar(16)		not null,
+    dni				char(9),
+    telefono		integer(9)		not null,
+    fecha_nac		date			not null,
+    genero			char(1)			not null	check(genero in ('H', 'M')),
+    titulacion		enum			("ESO", "Bachiller", "Formacion Profesional", "Universidad"),
+    
+    constraint pk_usuario primary key (dni)
+);
+
+DROP TABLE IF EXISTS COCHE;
+CREATE TABLE COCHE(
+	matricula		char(7),
+    marca			varchar(25)		not null,
+    modelo			varchar(25)		not null,
+    edad			integer			not null,
+    precio			float			not null,
+    dni_propietario	char(9),
+    
+    constraint pk_coche primary key (matricula),
+    constraint fk_coche foreign key (dni_propietario) references usuario(dni)
+);
---
+++ recursos/INSERTS-CONCESIONARIO.sql
@@ -0,0 +1,25 @@
+INSERT INTO USUARIO (nombre, contrasenia, dni, telefono, fecha_nac, genero, titulacion) VALUES 
+	('Jason', 'abcd', '79398882F', 688878533, '2004-08-01', 'H', 'Formacion Profesional'),
+    ('Ana García', 'contra123', '12345678A', 123456789, '1990-06-23', 'M', 'Universidad'),
+	('Pedro López', 'qwerty123', '98765432B', 987654321, '1995-08-12', 'H', 'Formacion Profesional'),
+	('Lucía Ruiz', 'abc123', '45678901C', 555555555, '1988-12-01', 'M', 'Bachiller'),
+	('Juan Sánchez', 'contraseña1', '23456789D', 111111111, '1975-04-18', 'H', 'ESO'),
+	('María Pérez', 'abcd1234', '34567890E', 222222222, '1999-11-30', 'M', 'Universidad'),
+	('Antonio Gómez', 'clave123', '56789012F', 333333333, '1985-07-10', 'H', 'Formacion Profesional'),
+	('Elena Rodríguez', 'contraseña2', '78901234G', 444444444, '1992-03-15', 'M', 'Bachiller'),
+	('Miguel Fernández', 'qwerty', '01234567H', 555555555, '1978-08-28', 'H', 'ESO'),
+	('Laura Torres', 'abc1234', '45678901I', 666666666, '1994-12-11', 'M', 'Universidad'),
+	('Javier Soto', 'clave', '23456789J', 777777777, '1980-01-21', 'H', 'Formacion Profesional');
+    
+INSERT INTO COCHE (matricula, marca, modelo, edad, precio, dni_propietario) VALUES
+	('6969SEX', 'Bugatti', 'Shiron', 0, 69420, null),
+    ('1234ABC', 'Renault', 'Clio', 2, 10000.50, null),
+	('5678DEF', 'Seat', 'Ibiza', 1, 8500.75, null),
+	('9012GHI', 'Ford', 'Focus', 3, 12000.00, null),
+	('3456JKL', 'Peugeot', '208', 2, 9500.25, null),
+	('7890MNO', 'Volkswagen', 'Golf', 4, 15000.00, null),
+	('ABCDPQR', 'Audi', 'A3', 2, 13000.50, null),
+	('EFGHSTU', 'BMW', 'Serie 1', 1, 18000.75, null),
+	('IJKLVWX', 'Mercedes-Benz', 'Clase A', 3, 20000.00, null),
+	('YZ01234', 'Opel', 'Corsa', 2, 9000.25, null),
+	('5678AAA', 'Toyota', 'Yaris', 1, 10000.00, null);
